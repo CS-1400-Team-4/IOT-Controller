2674e062ea0cc71a6aba4e0afcae8a6d15e0f1a2
Abigail Moore <abigailmmoore00@gmail.com>
2024-12-03 15:20:58 -0700

    Reworked device logic storage to be better

--- src/main/java/IOTController.java
+++ src/main/java/IOTController.java
@@ -1,69 +1,70 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public abstract class IOTController {
     public record InputState(int temperature, int time, int month, boolean isSomeoneHome) {}
 
     @FunctionalInterface
-    public interface IOTDevice {
+    public interface IOTLogic {
         boolean isActive(InputState newState, InputState previousState);
     }
+
+    public record IOTDevice(String name, String activeMessage, String inactiveMessage, IOTLogic logic) {}
 
     public static final int SET_POINT_TEMP = 70;
 
     // If it is between 9am and 6pm, and it's not too hot, open blinds
-    // Active means open
-    public static IOTDevice BLINDS = (current, prev) -> current.time() > 9 && current.time() < 18 && current.temperature() < SET_POINT_TEMP;
+    public static IOTDevice BLINDS = new IOTDevice("Blinds", "open", "closed",
+            (current, prev) -> current.time() > 9 && current.time() < 18 && current.temperature() < SET_POINT_TEMP);
 
     // If it is in the cold half of the year, heat home to set point if someone is home
     // Checks current and previous temperatures to avoid fast cycling
-    // Active means on
-    public static IOTDevice HEATER = (current, prev) -> {
-        if (current.temperature() < SET_POINT_TEMP && prev.temperature() < SET_POINT_TEMP
-                && (current.month() <= 4 || current.month() >= 10)
-                && current.isSomeoneHome())
-        {
-            return true;
-        }
+    public static IOTDevice HEATER = new IOTDevice("Heater", "running", "off",
+            (current, prev) -> {
+                if (current.temperature() < SET_POINT_TEMP && prev.temperature() < SET_POINT_TEMP
+                        && (current.month() <= 4 || current.month() >= 10)
+                        && current.isSomeoneHome())
+                {
+                    return true;
+                }
 
-        return false;
-    };
+                return false;
+            });
 
     // If it is in the warm half of the year, cool home to set point if someone is home
     // Checks current and previous temperatures to avoid fast cycling
-    // Active means on
-    public static IOTDevice AC = (current, prev) -> {
-        if (current.temperature() > SET_POINT_TEMP && prev.temperature() > SET_POINT_TEMP
-                && current.month() > 4 && current.month() < 10
-                && current.isSomeoneHome())
-        {
-            return true;
-        }
+    public static IOTDevice AC = new IOTDevice("Air Conditioning", "running", "off",
+            (current, prev) -> {
+                if (current.temperature() > SET_POINT_TEMP && prev.temperature() > SET_POINT_TEMP
+                        && current.month() > 4 && current.month() < 10
+                        && current.isSomeoneHome())
+                {
+                    return true;
+                }
 
-        return false;
-    };
+                return false;
+            });
 
     // Turn lights on if people are home in the evening
-    // Active means on
-    public static IOTDevice LIGHTS = (current, prev) -> {
-        // Turn on lights immediately if someone gets home, but leave them on briefly after leaving to
-        // avoid accidentally shutting lights off on anyone
-        if (prev.isSomeoneHome() || current.isSomeoneHome()) {
-            return current.time() > 18 && current.time() < 22;
-        }
+    public static IOTDevice LIGHTS = new IOTDevice("Lights", "on", "off",
+            (current, prev) -> {
+                // Turn on lights immediately if someone gets home, but leave them on briefly after leaving to
+                // avoid accidentally shutting lights off on anyone
+                if (prev.isSomeoneHome() || current.isSomeoneHome()) {
+                    return current.time() > 18 && current.time() < 22;
+                }
 
-        return false;
-    };
+                return false;
+            });
 
-    public static Map<IOTDevice, String> devices = new HashMap<>();
+    public static ArrayList<IOTDevice> devices = new ArrayList<>();
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        devices.put(BLINDS, "Blinds");
-        devices.put(HEATER, "Heater");
-        devices.put(AC, "A/C");
-        devices.put(LIGHTS, "Lights");
+        devices.add(BLINDS);
+        devices.add(HEATER);
+        devices.add(AC);
+        devices.add(LIGHTS);
 
         InputState previousState = null;
 
@@ -76,9 +77,9 @@ public abstract class IOTController {
             System.out.println("Current state is: " + currentState);
             System.out.println("Previous state is: " + previousState);
 
-            for (var device : devices.entrySet()) {
-                boolean isActive = device.getKey().isActive(currentState, previousState);
-                System.out.println("Device (" + device.getValue() + ") is " + (isActive ? "active" : "inactive"));
+            for (IOTDevice device : devices) {
+                boolean isActive = device.logic.isActive(currentState, previousState);
+                System.out.println("Device (" + device.name + ") is " + (isActive ? device.activeMessage : device.inactiveMessage));
             }
 
             previousState = currentState;
