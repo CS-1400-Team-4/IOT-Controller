4b644bbda5d28895fc8170882de9fb9edd848acc
Abigail Moore <abigailmmoore00@gmail.com>
2024-12-03 14:58:03 -0700

    Added user interaction

--- src/main/java/IOTController.java
+++ src/main/java/IOTController.java
@@ -1,4 +1,6 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public abstract class IOTController {
     public record InputState(int temperature, int time, int month, boolean isSomeoneHome) {}
@@ -11,24 +13,13 @@ public abstract class IOTController {
     public static final int SET_POINT_TEMP = 70;
 
     // If it is between 9am and 6pm, and it's not too hot, open blinds
+    // Active means open
     public static IOTDevice BLINDS = (current, prev) -> current.time() > 9 && current.time() < 18 && current.temperature() < SET_POINT_TEMP;
 
     // If it is in the cold half of the year, heat home to set point if someone is home
     // Checks current and previous temperatures to avoid fast cycling
+    // Active means on
     public static IOTDevice HEATER = (current, prev) -> {
-        if (current.temperature() < SET_POINT_TEMP && prev.temperature() < SET_POINT_TEMP
-                && current.month() > 4 && current.month() < 10
-                && current.isSomeoneHome())
-        {
-            return true;
-        }
-
-        return false;
-    };
-
-    // If it is in the warm half of the year, cool home to set point if someone is home
-    // Checks current and previous temperatures to avoid fast cycling
-    public static IOTDevice AC = (current, prev) -> {
         if (current.temperature() < SET_POINT_TEMP && prev.temperature() < SET_POINT_TEMP
                 && (current.month() <= 4 || current.month() >= 10)
                 && current.isSomeoneHome())
@@ -39,7 +30,22 @@ public abstract class IOTController {
         return false;
     };
 
+    // If it is in the warm half of the year, cool home to set point if someone is home
+    // Checks current and previous temperatures to avoid fast cycling
+    // Active means on
+    public static IOTDevice AC = (current, prev) -> {
+        if (current.temperature() > SET_POINT_TEMP && prev.temperature() > SET_POINT_TEMP
+                && current.month() > 4 && current.month() < 10
+                && current.isSomeoneHome())
+        {
+            return true;
+        }
+
+        return false;
+    };
+
     // Turn lights on if people are home in the evening
+    // Active means on
     public static IOTDevice LIGHTS = (current, prev) -> {
         // Turn on lights immediately if someone gets home, but leave them on briefly after leaving to
         // avoid accidentally shutting lights off on anyone
@@ -50,12 +56,63 @@ public abstract class IOTController {
         return false;
     };
 
-    public static ArrayList<IOTDevice> devices = new ArrayList<>();
+    public static Map<IOTDevice, String> devices = new HashMap<>();
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        devices.add(BLINDS);
-        devices.add(HEATER);
-        devices.add(AC);
-        devices.add(LIGHTS);
+        devices.put(BLINDS, "Blinds");
+        devices.put(HEATER, "Heater");
+        devices.put(AC, "A/C");
+        devices.put(LIGHTS, "Lights");
+
+        InputState previousState = null;
+
+        do {
+            InputState currentState = getInputStateFromUser();
+            if (previousState == null) {
+                previousState = currentState;
+            }
+
+            System.out.println("Current state is: " + currentState);
+            System.out.println("Previous state is: " + previousState);
+
+            for (var device : devices.entrySet()) {
+                boolean isActive = device.getKey().isActive(currentState, previousState);
+                System.out.println("Device (" + device.getValue() + ") is " + (isActive ? "active" : "inactive"));
+            }
+
+            previousState = currentState;
+        } while(checkForContinue());
+    }
+
+    public static InputState getInputStateFromUser() {
+        System.out.println("What is the current time? Input as a number from 0-23: ");
+        int time = scanner.nextInt();
+
+        while (time < 0 || time > 23) {
+            System.out.println("Please enter a number from 0-23: ");
+            time = scanner.nextInt();
+        }
+
+        System.out.println("What is the current month? Input as a number from 0-12: ");
+        int month = scanner.nextInt();
+
+        while (month < 0 || month > 12) {
+            System.out.println("Please enter a number from 0-12: ");
+            month = scanner.nextInt();
+        }
+
+        System.out.println("What is the current temperature? Input as a number in Fahrenheit: ");
+        int temperature = scanner.nextInt();
+
+        System.out.println("Is someone home right now? Y/N");
+        boolean isSomeoneHome = scanner.next().equalsIgnoreCase("y");
+
+        return new InputState(temperature, time, month, isSomeoneHome);
+    }
+
+    public static boolean checkForContinue() {
+        System.out.println("Do you want to continue? Y/N");
+        return scanner.next().equalsIgnoreCase("y");
     }
 }
